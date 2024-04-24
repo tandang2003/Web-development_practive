eb87b09fa56a526be1bdca39ddfdfe5e26f76fc3
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-04-24 11:39:14 +0700

    adding ip2Location properties and getNationlity method to get location with ip input

--- src/main/java/com/nhom44/ip2location/Ip2Location.java
+++ src/main/java/com/nhom44/ip2location/Ip2Location.java
@@ -1,2 +1,45 @@
-package com.nhom44.ip2location;public class Ip2Location {
+package com.nhom44.ip2location;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javax.script.ScriptContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Ip2Location {
+    public static final String Default_URL = "https://api.ip2location.io/?key=";
+
+    public static String getNationality(String ip) throws IOException {
+        String url = Default_URL + ip2LocationProperties.getIp2Location() + "&ip=" + ip;
+        URL urlObj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            Scanner scanner = new Scanner(connection.getInputStream());
+            StringBuilder jsonResponse = new StringBuilder();
+
+            while (scanner.hasNextLine()) {
+                jsonResponse.append(scanner.nextLine());
+            }
+
+            scanner.close();
+            String geolocationInfo = jsonResponse.toString();
+            JsonObject jsonObject = new Gson().fromJson(geolocationInfo, JsonObject.class);
+            geolocationInfo = jsonObject.get("city_name").getAsString()+", "+jsonObject.get("country_name").getAsString();
+            return geolocationInfo;
+        }
+        return "Ip undefined";
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getNationality("38.134.243.196"));
+    }
+
 }
