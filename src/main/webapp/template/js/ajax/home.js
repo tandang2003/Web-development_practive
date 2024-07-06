90dd8101368f414f175721c6b398dc7c1b5e60ab
Minh Chien <21130010@st.hcmuaf.edu.vn>
2024-07-06 20:54:43 +0700

    update fix conflict user

--- src/main/webapp/template/js/ajax/home.js
+++ src/main/webapp/template/js/ajax/home.js
@@ -1,102 +1,41 @@
-
 // page on ready
 $(document).ready(function () {
-    console.log("service page")
     getServices("home", "services-container");
-    // $.ajax({
-    //     url: '/api/home/data',
-    //     type: 'Get',
-    //     success: function (data) {
-    //
-    //         let slides = JSON.parse(data);
-    //         let slide = document.getElementById('slide-container');
-    //         slide.innerHTML = "";
-    //         slides.data.forEach((x, index) => {
-    //
-    //             let active = index === 0 ? "active" : "";
-    //             slide.innerHTML += `<div class="carousel-item ${active} w-100">
-    //             <img class="d-block w-100 " src="${x.avatar}"
-    //                  alt="${x.title}">`
-    //         })
-    //     },
-    // })
+
     $.ajax({
         url: '/api/home/data', type: 'Get',
         success: function (data) {
-            console.log(data)
-            let slides = JSON.parse(data);
-            console.log(slides)
-            slides.data.forEach((x, index) => {
+            let res = JSON.parse(data);
+            let slides = res['sliders'];
+            slides.forEach((x, index) => {
                 let active = index === 0 ? "active" : "";
                 $('#slide-container').append(`<div class="carousel-item ${active} w-100">
-                <img class="d-block w-100 " src="${x.avatar}"
-                     alt="${x.title}">`)
+                        <img class="d-block w-100 " src="${x.avatar}"
+                             alt="${x.title}">`)
             })
-            console.log($('#slide-container').html())
-        },
-    })
-    $.ajax({
-        url: "/api/home/categories",
-        type: 'Get',
-        success: function (data) {
-            let categories = JSON.parse(data);
-            console.log(123)
-            let categoryContainer = document.getElementById('category-container');
-            categoryContainer.innerHTML = "";
-            categories.data.forEach((x, index) => {
-                let active = index === 0 ? "active" : "";
-                categoryContainer.innerHTML += `<li class="category-item">
+            let categories = res['categories'];
+            categories.forEach((x, index) => {
+                $('#category-container').append(`<li class="category-item">
                          <button class="item-selector "
                                    role="tab" onclick="getProject(${x.id})"
                                    title="${x.name}">
                               XÂY DỰNG ${x.name}
                           </button>`)
-                console.log(x.id)
             })
             let list = res['projects'];
             list.forEach((x, index) => {
                 let project = drawProject(x);
                 $('#project-container').append(project);
             })
-            getProject(categories.data[0].id,false)
-        },
+        }
+        ,
+        error: function (data) {
+        }
     })
 })
 
 
-function saveContact() {
-    $.ajax({
-        url: '/api/contact/save', type: 'Post', dataType: 'json', data: {
-            fullName: $('#fullName').val(),
-            email: $('#email').val(),
-            address: $('#address').val(),
-            phone: $('#phone').val(),
-            content: $('#content').val(),
-        }, success: function (data) {
-            delayNotify(1000, data.message);
-            if (data.name == 'success') {
-                setTimeout(() => {
-                    window.location.reload();
-                }, 1000);
-            }
-        }, error: function (data) {
-            //bắt lỗi email
-            delayNotify(10000, "Vui lòng nhập đúng định dạng");
-            if (data.name == 'error') {
-                setTimeout(() => {
-                    window.location.reload();
-                }, 1000);
-            }
-        }
-
-    })
-}
-
-
-function getProject(id, notLoad=true) {
-    if(notLoad){
-        popularProject(id)
-    }
+function getProject(id, notLoad = true) {
     $.ajax({
         url: '/api/home/projects/' + id, type: 'Get', // dataType: 'json',
         // data: {id: id},
@@ -107,70 +46,36 @@ function getProject(id, notLoad=true) {
             let project = "";
             containter.innerHTML = "";
             for (const x of list) {
-               project += drawProject(x);
+                project += drawProject(x);
             }
             containter.innerHTML = project;
-            // console.log(project);
 
-        },
-        error: function (data) {
-            console.log(data);
+        }, error: function (data) {
         }
     })
 }
+
 function drawProject(x) {
     let project = "";
     project += '<div class="col-lg-3 col-md-4 col-sm-6 mb-4 overflow-hidden position-relative projectCard-container">' +
         '<div' + ' class="bg-image hover-image hover-zoom ripple shadow-1-strong rounded-5 w-100 d-block">';
 
 
-function like(project) {
-    let id = $(project).parent().find('.project-id').val();
-    console.log(id);
-    $.ajax({
-        url: "/api/save_project/"+id,
-        type: "GET",
-        success: function (response) {
-            console.log(response);
-            let resp = JSON.parse(response);
-            if (resp.name == 'save') {
-                likeLoging(id,true,true)
-                project.classList.replace("fa-regular", "fa-solid")
-            } else if (resp.name == 'delete') {
-                likeLoging(id,false)
-                project.classList.replace("fa-solid", "fa-regular")
-                //= "fa-solid fa-bookmark position-absolute";
-                // console.log(p);
-            }
-        },
-        error: function (response) {
-            likeLoging(id,false,true)
-            console.log(response.responseText)
-            let resp = JSON.parse(response.responseText);
-            window.location.href = resp.data;
-        }
-    })
+    if (x.isSave) project += ' <i class="fa-solid fa-bookmark position-absolute" onclick="like(this)" style="z-index: 1000"></i>'
+    else
+        project += '<i class="fa-regular fa-bookmark position-absolute" onclick="like(this)" style="z-index: 1000"></i>';
+    project += '<a href="/post/project/' + x.id + '">' +
+        '<img src="' + x.avatar + '"' + ' class="w-100">' +
+        ' <input type="hidden" class="project-id" value=' + x.id + '>' +
+        ' <div class="w-100 position-absolute projectCard-content">' +
+        '  <div class="mask justify-content-center d-flex h-100"' + ' style="background-color: rgba(48, 48, 48, 0.72);">' +
+        '<div class="align-items-center flex-column d-flex w-100">' +
+        ' <h6 class="text-white text-center pl-2 pr-2 projectTitle-center text-uppercase">' + x.title + '</h6>' +
+        '<p class="text-white p-0 id-project">' +
+        '<strong>MDA:' + x.id + '</strong>' +
+        '</p>' + '<p class="text-white p-4 vanBan">' + x.description + '</p>' +
+        '</div>' + '</div></div></a></div></div>'
+
+    return project;
 }
 
-
-function fetchErr(name, mess) {
-    console.log(name, mess)
-    switch (name) {
-        case 'email':
-            let email = document.getElementById('email');
-            email.classList.add('border-danger');
-            email.classList.add('text-danger');
-            email.value = "";
-            email.setAttribute('value', "");
-            // email.setAttribute('placeholder', mess);
-            break;
-    }
-}
-
-
-let fullName = document.getElementById('email');
-fullName.addEventListener('click', function () {
-    fullName.classList.remove('border-danger');
-    fullName.classList.remove('text-danger');
-    fullName.setAttribute('placeholder', "Email");
-})
--- src/main/webapp/views/public/project.jsp
+++ src/main/webapp/views/public/project.jsp
@@ -53,9 +53,9 @@
                         <div class="form-outline">
                             <select name="service" id="serviceId" class="form-control">
                                 <option value="">Loại dịch vụ</option>
-<%--                                <c:forEach var="service" items="${sessionScope.services}">--%>
-<%--                                    <option value="${service.id}">${service.name}</option>--%>
-<%--                                </c:forEach>--%>
+                                <%--                                <c:forEach var="service" items="${sessionScope.services}">--%>
+                                <%--                                    <option value="${service.id}">${service.name}</option>--%>
+                                <%--                                </c:forEach>--%>
                             </select>
                         </div>
                     </div>
@@ -63,9 +63,9 @@
                         <div class="form-outline">
                             <select name="address" id="provinceId" class="form-control">
                                 <option value="" selected>Chọn tỉnh thành</option>
-<%--                                <c:forEach items="${provinces}" var="province">--%>
-<%--                                    <option value="${province.id}">${province.name}</option>--%>
-<%--                                </c:forEach>--%>
+                                <%--                                <c:forEach items="${provinces}" var="province">--%>
+                                <%--                                    <option value="${province.id}">${province.name}</option>--%>
+                                <%--                                </c:forEach>--%>
                             </select>
                         </div>
                     </div>
@@ -74,9 +74,9 @@
                         <div class="form-outline">
                             <select name="area" id="area" class="form-control">
                                 <option value="">Diện tích</option>
-<%--                                <c:forEach items="${acreages}" varStatus="loop" var="area">--%>
-<%--                                    <option value="${loop.index+1}">${area}</option>--%>
-<%--                                </c:forEach>--%>
+                                <%--                                <c:forEach items="${acreages}" varStatus="loop" var="area">--%>
+                                <%--                                    <option value="${loop.index+1}">${area}</option>--%>
+                                <%--                                </c:forEach>--%>
                             </select>
                         </div>
                     </div>
@@ -84,9 +84,9 @@
                         <div class="form-outline">
                             <select name="price" id="price" class="form-control">
                                 <option value="">Kinh phí</option>
-<%--                                <c:forEach items="${prices}" varStatus="loop" var="price">--%>
-<%--                                    <option value="${loop.index+1}">${price.strType}</option>--%>
-<%--                                </c:forEach>--%>
+                                <%--                                <c:forEach items="${prices}" varStatus="loop" var="price">--%>
+                                <%--                                    <option value="${loop.index+1}">${price.strType}</option>--%>
+                                <%--                                </c:forEach>--%>
                                 </option>
 
                             </select>
@@ -137,6 +137,47 @@
 <script src="<c:url value="/template/js/main.js"/>"></script>
 <script src="<c:url value='/template/js/ajax/saveProject.js'/>"></script>
 
+<script>
+    $(document).ready(function () {
+        $.ajax({
+            url: "/api/project",
+            type: "GET",
+            dataType: "json",
+            success: function (response) {
+                console.log('project');
+                console.log(response);
+                let data = response;
+                data = JSON.parse(data.data);
+                let tag='';
+                for (let s of data.services) {
+                    tag += '<option value="' + s.id + '">' + s.name + '</option>';
+                }
+                $('#serviceId').append(tag);
+                tag = '';
+                for (let c of data.categories) {
+                    tag += '<option value="' + c.id + '">' + c.name + '</option>';
+                }
+                $('#categoryId').append(tag);
+                tag = '';
+                for (let p of data.provinces) {
+                    tag += '<option value="' + p.id + '">' + p.name + '</option>';
+                }
+                $('#provinceId').append(tag);
+                tag = '';
+                for (let p of data.prices) {
+                    tag += '<option value="' + p.amount + '">' + p.strType + '</option>';
+                }
+                $('#price').append(tag);
+                tag = '';
+                for (let a of data.acreages) {
+                    tag += '<option value="' + a + '">' + a + '</option>';
+                }
+                $('#area').append(tag);
+
+            },
+        })
+    })
+</script>
 <script>
     $(document).ready(function () {
         $.ajax({
